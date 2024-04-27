0a6b86165cf09636dc141da25b1749956386940b
noe <noe.jaraq@gmail.com>
2024-04-27 18:01:56 -0500

    first commit

---
+++ app/src/main/java/com/example/laboratory4/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.laboratory4;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.laboratory4.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.enterButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AppActivity.class);
+            startActivity(intent);
+        });
+    }
+}
